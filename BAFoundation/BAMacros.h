7ac8ccbb4ba385253880001b4f7e37eb5da40561
Brent Gulanowski <brent@shopify.com>
2015-01-21 21:56:11 -0500

    Move macros from BAScene.

--- BAFoundation/BAMacros.h
+++ BAFoundation/BAMacros.h
@@ -8,6 +8,19 @@
  */
 
 
+#define sine sinf
+#define cosine cosf
+#define absolute fabsf
+#define BAInt long
+#define BAUInt unsigned long
+#define NAN_OFFSET LONG_MAX+1  // -1 signed long
+#define ULPS_DELTA 0x0fff
+#ifdef FLT_EPSILON
+#define EPSILON FLT_EPSILON
+#else
+#define EPSILON __FLT_EPSILON__
+#endif
+
 #define BALog(x) NSLog(@"%@ - %@\n\t%@", NSStringFromClass([self class]), NSStringFromSelector(_cmd), (x))
 
 #define BARandomLongLong()             (((long long)random() << 32) | (long long)random())
@@ -19,4 +32,4 @@
 #endif
 #define BARandomBool()                 (random() & 1)
 #define BARandomSignedness()           (BARandomBool() * 2 - 1)
-#define BARandomCGFloatInRange(_a, _b) (BARandomCGFloat() * fabs(_b - _a) + MIN(_a,_b))
+#define BARandomCGFloatInRange(_a, _b) (BARandomCGFloat() * absolute(_b - _a) + MIN(_a,_b))
