848875aac5c4389f5e0e902c3b2afad3383b5d25
Brent Gulanowski <bgulanowski@gmail.com>
2015-05-16 17:16:36 -0400

    Add framework common header.

---
+++ BAFoundation/BAFoundation.h
@@ -0,0 +1,23 @@
+//
+//  BAFoundation.h
+//  BAFoundation
+//
+//  Created by Brent Gulanowski on 2015-05-12.
+//  Copyright (c) 2015 Lichen Labs. All rights reserved.
+//
+
+#import <BAFoundation/BABitArray.h>
+#import <BAFoundation/BABitArray+Rectangles.h>
+#import <BAFoundation/BASampleArray.h>
+#import <BAFoundation/BASparseArray.h>
+#import <BAFoundation/BASparseBitArray.h>
+#import <BAFoundation/BASparseSampleArray.h>
+
+#import <BAFoundation/BACoreDataManager.h>
+#import <BAFoundation/BARelationshipProxy.h>
+#import <BAFoundation/BAUUID.h>
+
+#import <BAFoundation/BANoise.h>
+
+#import <BAFoundation/BAGraphNode.h>
+#import <BAFoundation/BATextIOHandler.h>
